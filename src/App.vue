1c1df9e3d8af69c97d535bd71c0983eb4f46814a
dsalex1 <dsalex1@ymail.com>
2024-02-16 13:29:20 +0100

    vue boilerplate

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Renderer from "./Managers/Renderer.vue";
+import EntityManager from "./Managers/EntityManager";
+import LevelManager from "./Managers/LevelManager";
+import GamePhysics from "./Managers/GamePhysics";
+
+const entityManager = new EntityManager();
+
+const levelManager = new LevelManager(entityManager);
+const gamePhysics = new GamePhysics(entityManager);
+
+levelManager.loadLevel();
+
+setInterval(() => {
+  gamePhysics.tick();
+}, 10);
+</script>
+
+<template>
+  <Renderer :entityManager="entityManager"></Renderer>
+</template>
+
+<style scoped></style>
